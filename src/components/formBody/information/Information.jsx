ef2ad725d0f9b08e225b0939ff8a9bdab9177302
NTHau <nthau18@apcs.fitus.edu.vn>
2022-04-25 13:16:46 +0700

    useEffect

--- src/components/formBody/information/Information.jsx
+++ src/components/formBody/information/Information.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import "./information.css";
@@ -6,12 +6,10 @@ import UploadImages from "../../uploader/UploadImages";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 
-const options = ["CMT/TCC/Passport", "Bằng lái xe", "Đăng ký xe", "Văn bản"];
-
 const cccd = [
   "so",
   "hoVaTen",
-  "ngayThangNamSinh",
+  "ngaySinh",
   "gioiTinh",
   "quocTich",
   "queQuan",
@@ -20,17 +18,46 @@ const cccd = [
 ];
 
 const blx = [
+  "so",
   "hoVaTen",
   "ngaySinh",
   "quocTich",
   "noiCuTru",
   "hang",
   "coGiaTriDen",
+  "ngayCap",
+];
+
+const dkx = [
+  "soKhung",
+  "tenChuXe",
+  "diaChi",
+  "nhanHieu",
+  "soLoai",
+  "loaiXe",
+  "dungTich",
+  "mauSon",
+  "taiTrong",
+  "soChoNgoi",
+  "giaTriDen",
+  "bienSoXe",
+  "lanDKDau",
+  "ngayThangNam",
+];
+
+const vb = ["Văn bản"];
+
+const options = [
+  { id: 0, displayName: "CMT/TCC/Passport", fields: cccd },
+  { id: 1, displayName: "Bằng lái xe", fields: blx },
+  { id: 2, displayName: "Đăng ký xe", fields: dkx },
+  { id: 3, displayName: "Văn bản", fields: vb },
 ];
 
 const Information = (props) => {
   const onPreview = () => {
-    if (formik.values.noiDung !== false && formik.values.image.name) {
+    console.log(formik.values.cotNoiDung);
+    if (formik.values.cotNoiDung !== [] && formik.values.image.name) {
       props.onBindingPreview(formik.values);
     } else alert("Điền đầy đủ các mục bắt buộc");
   };
@@ -42,22 +69,26 @@ const Information = (props) => {
   const formik = useFormik({
     initialValues: {
       fileType: "image",
-      vanBan: "",
-      noiDung: false,
+      loaiGiayTo: 3,
+      cotNoiDung: options[3].fields,
       image: {},
     },
     validationSchema: Yup.object({
       fileType: Yup.string().required("Required!"),
-      noiDung: Yup.boolean(),
+      cotNoiDung: Yup.array().required("Required!"),
     }),
     onSubmit: (values) => {
-      if (formik.values.noiDung !== false && formik.values.image.name) {
+      if (formik.values.cotNoiDung !== [] && formik.values.image.name) {
         props.onBindingPreview(formik.values);
         alert("Biểu mẫu đã lưu thành công!");
         formik.resetForm();
       } else alert("Điền đầy đủ các mục bắt buộc");
     },
   });
+
+  useEffect(() => {
+    formik.values.cotNoiDung = options[formik.values.loaiGiayTo].fields;
+  }, [formik.values.loaiGiayTo]);
 
   const handleImage = (img) => {
     formik.setFieldValue("image", {
@@ -97,13 +128,13 @@ const Information = (props) => {
               select
               label="Chọn loại văn bản"
               onChange={formik.handleChange}
-              value={formik.values.vanBan}
-              name="vanBan"
+              value={formik.values.loaiGiayTo}
+              name="loaiGiayTo"
               sx={{ width: "100%" }}
             >
               {options.map((option) => (
-                <MenuItem key={option} value={option}>
-                  {option}
+                <MenuItem key={option.id} value={option.id}>
+                  {option.displayName}
                 </MenuItem>
               ))}
             </TextField>
@@ -113,22 +144,28 @@ const Information = (props) => {
         <div className="formField">
           <label className="required">
             Cột thông tin sẽ lưu <span className="warning-sign">(*)</span>
-            {formik.values.noiDung === false || !formik.values.image.name ? (
+            {formik.values.cotNoiDung === [] || !formik.values.image.name ? (
               <p className="warning-message">
                 {"Cột thông tin sẽ lưu không được bỏ trống"}
               </p>
             ) : null}
           </label>
           <div className="formField__input">
-            <input
-              type="checkbox"
-              id="noiDung"
-              name="noiDung"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-
-            <label>Nội dung</label>
+            <div className="formField__input-multiCheck">
+              {options[formik.values.loaiGiayTo].fields.map((field) => (
+                <label key={field}>
+                  <input
+                    type="checkbox"
+                    defaultChecked="checked"
+                    className="multi-input"
+                    name="cotNoiDung"
+                    value={field}
+                    onChange={formik.handleChange}
+                  />
+                  {field}
+                </label>
+              ))}
+            </div>
           </div>
         </div>
 
